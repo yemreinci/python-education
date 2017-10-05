da55e57bb95c500aa6bf1a196449cfa2441a6b43
Yunus Emre İnci <yemreinci@mail.com>
2017-10-05 14:04:59 +0300

    codes

---
+++ input_and_variables.py
@@ -0,0 +1,14 @@
+
+
+# input fonksiyonu konsola içine yazdığımız string i yazdırır ve ardından kullanıcıdan bir
+# karakter dizisi alır.
+x = input('birseyler yazin ve enter a basin: ')
+
+print(x)
+
+x = input('bir sayi giriniz: ')
+
+print('girdiginiz sayinin 2 kati: ' + str(2*x))
+
+
+

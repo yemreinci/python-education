35930ba77a1f16057534d667daa1e94d83f13cc0
Yunus Emre İnci <yemreinci@mail.com>
2017-10-20 18:22:24 +0300

    add hackerrank challenges

---
+++ hafta3/odevler/extra_challenges.txt.py
@@ -0,0 +1,7 @@
+Meraklı arkadaşlar için 2 challenge daha.
+Alttaki daha zor :)
+
+Soruları görebilmek için siteye üye olmanız gerekiyor olabilir.
+
+https://www.hackerrank.com/contests/w31/challenges/beautiful-word
+https://www.hackerrank.com/contests/w33/challenges/twin-arrays

edd755b6e216d21417f09ffec7adcfc99830d4f3
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2017-10-27 17:54:06 +0300

    Fix fibonacci approximation

--- hafta4/ornekler/007_recursive_function.py
+++ hafta4/ornekler/007_recursive_function.py
@@ -1,3 +1,6 @@
+# Asagidaki approximation yanlis. Derste 1.618033^n olarak hatirladim ama ayrica bir de /sqrt(5) varmis.
+# b list'ini olusturan satira bu ifadeyi de eklerseniz percentage error'un cok daha azaldigini ve iki grafigin ust uste bindigini gorebilirsiniz.
+
 from matplotlib import pyplot as plt
 from math import sqrt
 
@@ -9,6 +12,7 @@ def fibo(n): # fibonaccinin n. elemanini bulur
 
 a = [fibo(i) for i in range(10)] # fibonacci sayilari
 b = [1.618033 ** i for i in range(10)] # altin oranin usleri
+#b = [1.618033 ** (i+1) / sqrt(5) for i in range(10)] # altin oranin usleri
 diffs = [(b[i]-a[i])/a[i]*100 for i in range(10)] # aradaki fark (yuzde olarak)
 
 print(a)
